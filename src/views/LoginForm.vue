db989e4a30fc1b69c1c8243d59cc74a1a59e4678
liorabel22 <60816560+liorabel22@users.noreply.github.com>
2024-06-11 22:16:55 +0300

    done

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,50 +1,63 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <BaseInput
-      label="Email"
-      type="email"
-      v-model="email"
-      :error="emailError"
-    />
+    <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
     <BaseInput
       label="Password"
       type="password"
+      v-model="password"
+      :error="passwordError"
     />
 
-    <BaseButton
-      type="submit"
-      class="-fill-gradient"
-    >
-    Submit
+    <BaseButton type="submit" class="-fill-gradient">
+      Submit
     </BaseButton>
   </form>
 </template>
 
 <script>
-import { useField } from 'vee-validate'
+import { useField, useForm } from "vee-validate";
 export default {
-  setup () {
-    function onSubmit () {
-      alert('Submitted')
+  setup() {
+    function onSubmit() {
+      alert("Submitted");
     }
 
-    const { value: email, errorMessage: emailError } = useField('email', (value) => {
-      if (!value) return 'This field is required'
+    const validations = {
+      email: value => {
+        if (!value) return "This field is required";
 
-      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      if (!regex.test(String(value).toLowerCase())) {
-        return 'Please enter a valid email address'
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!regex.test(String(value).toLowerCase())) {
+          return "Please enter a valid email address";
+        }
+
+        return true;
+      },
+      password: value => {
+        const requiredMessage = "This field is required";
+        if (value === undefined || value === null || value === "") {
+          return requiredMessage;
+        }
+
+        return true;
       }
+    };
 
-      return true
-    })
+    useForm({ validationSchema: validations });
+
+    const { value: email, errorMessage: emailError } = useField("email");
+    const { value: password, errorMessage: passwordError } = useField(
+      "password"
+    );
 
     return {
       onSubmit,
       email,
-      emailError
-    }
+      emailError,
+      password,
+      passwordError
+    };
   }
-}
+};
 </script>
