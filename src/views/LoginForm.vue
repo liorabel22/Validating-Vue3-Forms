8243cb9994188466df2ae2f729499d6c1cadeb06
liorabel22 <60816560+liorabel22@users.noreply.github.com>
2024-06-11 18:46:58 +0300

    done

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,34 +1,45 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <BaseInput
-      label="Email"
-      type="email"
-    />
+    <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput
-      label="Password"
-      type="password"
-    />
+    <BaseInput label="Password" type="password" />
 
-    <BaseButton
-      type="submit"
-      class="-fill-gradient"
-    >
-    Submit
+    <BaseButton type="submit" class="-fill-gradient">
+      Submit
     </BaseButton>
   </form>
 </template>
 
 <script>
+import { useField } from "vee-validate";
+
 export default {
-  setup () {
-    function onSubmit () {
-      alert('Submitted')
+  setup() {
+    function onSubmit() {
+      alert("Submitted");
     }
 
+    const { value: email, errorMessage: emailError } = useField(
+      "email",
+      value => {
+        if (!value) {
+          return "This field is required";
+        }
+
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!regex.test(String(value).toLowerCase())) {
+          return "Please enter a valid email address";
+        }
+
+        return true;
+      }
+    );
+
     return {
-      onSubmit
-    }
+      onSubmit,
+      email,
+      emailError
+    };
   }
-}
+};
 </script>
